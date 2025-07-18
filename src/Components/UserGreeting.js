90dbecc07ed68d4ad2e5232de8041b25303db456
pratiksha0024 <pratikshab609@gmail.com>
2025-07-18 21:18:31 +0530

    commited

--- src/Components/UserGreeting.js
+++ src/Components/UserGreeting.js
@@ -7,13 +7,32 @@ class UserGreeting extends Component{
             isLoggedIn:true
         }
     }
-    render()
-    {
+
+    render(){
+
+        // return this.state.isLoggedIn && <div> Thank you pratiksha for LoggedIn</div>
+        
+    //     return this.state.isLoggedIn ? (
+    //     <div> thank you </div> 
+    // ): (
+    //         <div> plz Log In </div>
+    // )
+
+        // let message
+        // if (this.state.isLoggedIn){
+        //     message = <dive> Thank you for log in</dive>
+        // }else{
+        //     message=<div> you are not logged in</div>
+        // }
+
+        // return <div> {message}</div>
+
            if (this.state.isLoggedIn){
             return <div> Hello Pratiksha</div>
            }else {}
             return <div> Welcome Guest</div>
         }
     
+
 }
-export default UserGreeting
+export default UserGreeting;
